e679ea69060656ff63ec699af774dada95e94c46
mani-create <68181215+mani-create@users.noreply.github.com>
2021-03-19 13:23:02 +0530

    Add files via upload

---
+++ MY DATA 1/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+  });
+
+
+  AOS.init({
+    offset :200,
+    duration : 1000
+  });
